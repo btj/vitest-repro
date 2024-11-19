f3ade14c117b488b18faec38430daa14bdc00297
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-11-19 13:36:34 +0100

    Add repro instruction

--- index.test.mjs
+++ index.test.mjs
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest';
 
 describe('Test Suite', () => {
     it('hello world!', () => {
-        console.log('Hello World!');
+        console.log('Hello World!'); // Put a breakpoint here and then Step Over; notice no output appears in the Test results pane.
     });
 });
